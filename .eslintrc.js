a013aa649929ed79cfe4b1fe161410c23ce141cf
Ben Garren <bngarren@gmail.com>
2023-11-15 11:10:00 -0500

    Upgrade eslint packages and prettier. Modified .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,21 @@
 module.exports = {
+  // This is required, or else ESLint will throw errors as it tries to parse TypeScript code as if it were regular JavaScript
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  root: true,
+  plugins: ["@typescript-eslint", "unused-imports"], // Plugins allow you to use rules, but don't enforce anything
   extends: [
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    //"plugin:@typescript-eslint/recommended-type-checked", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier
   ],
-  plugins: ["unused-imports"],
   parserOptions: {
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    project: "./tsconfig*.json",
+    tsconfigRootDir: __dirname,
+    ecmaVersion: "latest", // Allows for the parsing of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
   },
+  ignorePatterns: ['/*.js', '/*.ts'],
   rules: {
     // Ignores unused vars when in the context of destructuring or names preceded with _
     "@typescript-eslint/no-unused-vars": "off",
@@ -21,6 +28,7 @@ module.exports = {
         destructuredArrayIgnorePattern: "^_",
       },
     ],
+    "@typescript-eslint/no-unsafe-assignment": "off",
     // Some style for comments
     "spaced-comment": [
       "warn",
